3ac966eee425557a37252fca518887ec7b2ddd97
Simon Herrick <simon@herrick.io>
2023-11-18 08:02:16 +0000

    reset frontend

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,13 +1,13 @@
-// components/NavBar.js
+// src/components/NavBar.js
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">Edit Names</Navbar.Brand>
+      <Navbar.Brand className="mx-auto" href="#home">Scrum Tool</Navbar.Brand>
     </Navbar>
   );
-};
+}
 
 export default NavBar;
